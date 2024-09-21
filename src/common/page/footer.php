c9fd258b82b6a8a22617a17d2db26f699dd31750
Callan P Milne <callanpmilne@outlook.com>
2024-09-21 10:08:25 +1000

    Updated common page header and footer

--- src/common/page/footer.php
+++ src/common/page/footer.php
@@ -1,3 +1,17 @@
 <footer>
   Copyright &copy; <?php echo date("Y"); ?>
 </footer>
+
+<style>
+  footer {
+    border-top: 1px solid #ddd;
+    color: #00000095;
+    height: 4rem;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    padding: 0 1rem 5rem;
+    font-size: 0.88rem;
+  }
+</style>
--- src/common/page/header.php
+++ src/common/page/header.php
@@ -2,13 +2,75 @@
 <html lang="en-au">
 <head>
   <title>Lexar</title>
+  <link
+    rel="stylesheet"
+    href="/style.css" />
 </head>
 
 <body>
   <header>
-    <a href="/">Home</a>
-    |
-    <a href="/login">Login</a>
-    |
-    <a href="/404">404</a>
+    <ul id="topmenu">
+      <li>
+        <a 
+          href="/">
+          Home
+        </a>
+      </li>
+      <li>
+        <a
+          href="/login">
+          Login
+        </a>
+      </li>
+      <li>
+        <a 
+          href="/404">
+          404
+        </a>
+      </li>
+    </ul>
   </header>
+
+  <style>
+    header {
+      height: 2.5rem;
+      font-size: 0.9rem;
+      font-weight: 400;
+      background-color: #222;
+      box-shadow: 0px 0px 5px rgba(0,0,0,0.5);
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      justify-content: center;
+    }
+
+    #topmenu {
+      list-style-type: none;
+      padding: 0;
+      margin: 0;
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      justify-content: center;
+    }
+
+    #topmenu li {
+      display: flex;
+      color: #ffffff50;
+    }
+
+    #topmenu li::after {
+      content: '|';
+      display: flex;
+      margin: 0 0.5rem;
+    }
+
+    #topmenu li:last-of-type::after {
+      content: '';
+      margin: 0;
+    }
+
+    #topmenu li a {
+      color: #fff;
+    }
+  </style>
