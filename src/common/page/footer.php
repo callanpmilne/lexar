a85c698057b37340273c706ba734040a58dfa6da
Callan P Milne <callanpmilne@outlook.com>
2024-09-21 10:21:08 +1000

    Updated common footer

--- src/common/page/footer.php
+++ src/common/page/footer.php
@@ -1,5 +1,10 @@
 <footer>
-  Copyright &copy; <?php echo date("Y"); ?>
+  <section>
+    <p>Copyright &copy; <?php echo date("Y"); ?></p>
+    <p id="poweredby">
+      [ Powered by <a href="https://github.com/callanpmilne/lexar" target="_blank">Lexar</a> ]
+    </p>
+  </section>
 </footer>
 
 <style>
@@ -11,7 +16,35 @@
     flex-direction: row;
     align-items: center;
     justify-content: center;
-    padding: 0 1rem 5rem;
+    padding: 1rem 1rem 5rem;
     font-size: 0.88rem;
+  }
+
+  footer section {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+
+  footer section p {
+    margin: 0;
+  }
+
+  footer section p:first-of-type {
+    margin-top: 0;
+  }
+
+  footer section p:last-of-type {
+    margin-bottom: 0;
+  }
+
+  #poweredby {
+    margin-top: 0.5rem;
+    font-size: 0.7rem;
+  }
+
+  #poweredby a {
+    font-weight: 500;
   }
 </style>
