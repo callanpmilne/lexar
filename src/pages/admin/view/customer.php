e5d9c734e7896b302fde48e6081b9e90d4864dab
Callan P Milne <callanpmilne@outlook.com>
2024-09-27 00:56:31 +1000

    Admin View Customer Page

--- src/pages/admin/view/customer.php
+++ src/pages/admin/view/customer.php
@@ -25,15 +25,99 @@ $customer = fetchCustomer($CustomerID);
     </a>
   </p>
 
-  <ul>
-    <li>
-      <span class="label">ID:</span>
-      <?=$customer->ID?>
-    </li>
+  <div class="customer-page">
+    <div class="secondary-content">
+      <section>
+        <h2>Profile</h2>
 
-    <li>
-      <span class="label">Name:</span>
-      <?=$customer->Name?>
-    </li>
-  </ul>
+        <ul>
+          <li>
+            <span class="label">ID:</span>
+            <?=$customer->ID?>
+          </li>
+
+          <li>
+            <span class="label">Name:</span>
+            <?=$customer->Name?>
+          </li>
+        </ul>
+      </section>
+
+      <section>
+        <h2>Recent Transactions</h2>
+
+        <p>Recent Transactions...</p>
+      </section>
+    </div>
+
+    <div class="primary-content">
+      <section class="note-editor">
+        Add Note Editor Here to Add Notes Easily
+      </section>
+
+      <section>
+        <h2>Notes</h2>
+
+        <p>Notes...</p>
+      </section>
+
+      <section>
+        <h2>Interactions</h2>
+
+        <p>Interactions...</p>
+      </section>
+    </div>
+  </div>
 </main>
+
+<style>
+main div.customer-page {
+  display: flex;
+  width: 100%;
+  flex-direction: row-reverse;
+  align-items: flex-start;
+  justify-content: stretch;
+}
+
+main div.customer-page div.primary-content,
+main div.customer-page div.secondary-content {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: flex-start;
+}
+
+main div.customer-page div.secondary-content {
+  flex: 2;
+}
+
+main div.customer-page section {
+  margin-bottom: 2rem;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: stretch;
+}
+
+main div.customer-page div.primary-content {
+  padding-right: 2rem;
+  flex: 4;
+}
+
+main div.customer-page section.note-editor {
+  aspect-ratio: 16 / 6;
+  width: 100%;
+  background-color: rgba(0, 0, 0, 0.2);
+  border-radius: 1rem;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+  margin: 0;
+}
+</style>
