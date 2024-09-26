814f4b6dc83f54ab5338078dee006f9506e6753e
Callan P Milne <callanpmilne@outlook.com>
2024-09-27 07:24:07 +1000

    Customer Admin Page

--- src/pages/admin/view/customer.php
+++ src/pages/admin/view/customer.php
@@ -46,7 +46,37 @@ $customer = fetchCustomer($CustomerID);
       <section>
         <h2>Recent Transactions</h2>
 
-        <p>Recent Transactions...</p>
+        <table>
+          <thead>
+            <tr>
+              <th>
+                <input id=""
+                  name=""
+                  type="checkbox"
+                  value="" />
+              </th>
+
+              <th>Description</th>
+              <th>Date</th>
+              <th>Amount</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr>
+              <td>
+                <input id=""
+                  name=""
+                  type="checkbox"
+                  value="" />
+              </td>
+
+              <td>Short description of the payment</td>
+              <td><?=print(date('d/m/y'))?></td>
+              <td>US&dollar; 123.00</td>
+            </tr>
+          </tbody>
+        </table>
       </section>
     </div>
 
@@ -58,33 +88,136 @@ $customer = fetchCustomer($CustomerID);
       <section>
         <h2>Notes</h2>
 
-        <p>Notes...</p>
+        <table id="CustomerNotes">
+          <thead>
+            <tr>
+              <th>
+                <input id=""
+                  name=""
+                  type="checkbox"
+                  value="" />
+              </th>
+
+              <th>Author</th>
+
+              <th>Created</th>
+
+              <th class="wide">Note</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr>
+              <td>
+                <input id=""
+                  name=""
+                  type="checkbox"
+                  value="" />
+              </td>
+
+              <td>
+                <a href="/#">Admin</a>
+              </td>
+
+              <td>
+                <?=date('d/m/Y')?><br />
+                <?=date('h:i:sA')?>
+              </td>
+
+              <td class="wide">
+                <p>
+                  This is the note content body which contains the main text of the note.
+                </p>
+                <p>
+                  Most notes are expected to span multiple paragraphs and QA testing must be carried out to ensure it's functionality and reliability.
+                </p>
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </section>
 
       <section>
         <h2>Interactions</h2>
 
-        <p>Interactions...</p>
+        <table id="CustomerInteractions">
+          <thead>
+            <tr>
+              <th>
+                <input id=""
+                  name=""
+                  type="checkbox"
+                  value="" />
+              </th>
+
+              <th>Who</th>
+
+              <th>When</th>
+
+              <th class="wide">Interaction Details</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr>
+              <td>
+                <input id=""
+                  name=""
+                  type="checkbox"
+                  value="" />
+              </td>
+
+              <td>
+                <a href="/#">Admin</a>
+              </td>
+
+              <td>
+                <?=date('d/m/Y')?><br />
+                <?=date('h:i:sA')?>
+              </td>
+
+              <td class="wide">
+                <p>
+                  This is the note content body which contains the main text of the note.
+                </p>
+                <p>
+                  Most notes are expected to span multiple paragraphs and QA testing must be carried out to ensure it's functionality and reliability.
+                </p>
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </section>
     </div>
   </div>
 </main>
 
 <style>
+table#CustomerNotes {
+
+}
+
+table#CustomerNotes tr th,
+table#CustomerNotes tr td,
+table#CustomerInteractions tr th,
+table#CustomerInteractions tr td {
+  text-align: left;
+}
+
 main div.customer-page {
   display: flex;
-  width: 100%;
   flex-direction: row-reverse;
-  align-items: flex-start;
+  align-items: stretch;
   justify-content: stretch;
+  width: 100%;
 }
 
 main div.customer-page div.primary-content,
 main div.customer-page div.secondary-content {
   display: flex;
   flex-direction: column;
-  align-items: flex-start;
-  justify-content: flex-start;
+  align-items: stretch;
+  justify-content: stretch;
 }
 
 main div.customer-page div.secondary-content {
@@ -95,7 +228,7 @@ main div.customer-page section {
   margin-bottom: 2rem;
   display: flex;
   flex-direction: column;
-  align-items: flex-start;
+  align-items: stretch;
   justify-content: stretch;
 }
 
