9552eb17d29cadc3e3dd6fcb29e745253ed50c53
Callan Milne <callanpmilne@gmail.com>
2024-10-13 17:57:06 +1100

    Entity Type Code Generator

--- src/pages/admin/view/entity/type.php
+++ src/pages/admin/view/entity/type.php
@@ -45,7 +45,9 @@ $sourceFiles = $sourceFiles = generateSourceFiles(module: $module);
 
   <section class="split-content">
     <div class="content-section">
-      <section id="EntityTypeDetail">
+      <section 
+        id="EntityTypeDetail"
+        class="entity-type-admin-block">
         <h2><?=$type->Label?></h2>
         
         <ul>
@@ -74,33 +76,103 @@ $sourceFiles = $sourceFiles = generateSourceFiles(module: $module);
           </li>
         </ul>
       </section>
+
+      <section class="entity-type-admin-block">
+        <h2>Attributes</h2>
+        
+        <ul>
+          <li>
+            <span class="label">Unique ID</span>
+            ID
+          </li>
+
+          <li>
+            <span class="label">Parent ID</span>
+            Parent ID
+          </li>
+
+          <li>
+            <span class="label">Display Name</span>
+            Name
+          </li>
+        </ul>
+      </section>
     </div>
 
     <div class="content-section">
       <section id="EntityTypeExplorer">
-        <h2>Entity Type Explorer</h2>
+        <h2>Entity Type Code Generator</h2>
 
         <div class="entity-type-explorer">
-          <h3>Code Preview</h3>
+          <div class="file-browser">
+            <?php foreach ($sourceFiles as $uri => $src) : ?>
+              <div 
+                class="file-browser-file">
+                <?=$uri?>
+              </div>
+            <?php endforeach; ?>
+          </div>
+          
+          <div class="file-view">
+            <?php foreach ($sourceFiles as $uri => $src) : ?>
+              <div 
+                class="entity-type-code-preview">
+                <label class="output">
+                  <pre class="source-file-path"><?=$uri?></pre>
+                </label>
 
-          <?php foreach ($sourceFiles as $uri => $src) : ?>
-            <div 
-              class="entity-type-code-preview">
-              <label class="output">
-                <pre class="source-file-path"><?=$uri?></pre>
-              </label>
-
-              <?=highlight_string(
-                html_entity_decode($src), 
-                true
-              )?>
-            </div>
-          <?php endforeach; ?>
+                <?=highlight_string(
+                  html_entity_decode($src), 
+                  true
+                )?>
+              </div>
+            <?php endforeach; ?>
+          </div>
         </div>
       </section>
     </div>
   </section>
 </main>
+
+<script>
+  "use strict";
+  (function ($) {
+    var $document = $.document;
+    var $codePreviewEls = $document.querySelectorAll('.entity-type-code-preview');
+    var $fileBrowserFileEls = $document.querySelectorAll('.file-browser-file');
+
+    // Bind File Browser File Clicks
+    $fileBrowserFileEls.forEach(($el) => {
+      const srcFilePath = $el.innerText;
+      $el.addEventListener('click', () => {
+        showPreview(srcFilePath);
+      });
+    });
+    
+    // Hide the code previews
+    $codePreviewEls.forEach(($el) => {
+      console.log($el);
+      $el.classList.add('hidden');
+    });
+
+    $codePreviewEls[0].classList.remove('hidden');
+    
+    function showPreview (srcFilePath) {
+      $codePreviewEls.forEach(($el) => {
+        if (srcFilePath === $el.querySelector('pre.source-file-path').innerText) {
+          $el.classList.remove('hidden');
+          return;
+        }
+
+        if ($el.classList.contains('hidden')) {
+          return;
+        }
+
+        $el.classList.add('hidden');
+      });
+    }
+  })(window);
+</script>
 
 <style>
   section.split-content {
@@ -138,11 +210,21 @@ $sourceFiles = $sourceFiles = generateSourceFiles(module: $module);
     display: flex;
     flex: 1;
     flex-direction: column;
-    margin-bottom: 2rem;
   }
   
+  #EntityTypeExplorer div.entity-type-code-preview.hidden {
+    display: none;
+  }
+
   #EntityTypeExplorer div.entity-type-code-preview pre {
+    font-size: 0.7rem;
+    backdrop-filter: brightness(0.8) blur(50px);
     margin: 0;
+    flex: 1;
+    overflow: scroll;
+    padding: 0.5rem;
+    width: 100%;
+    box-sizing: border-box;
   }
   
   #EntityTypeExplorer div.entity-type-code-preview pre.source-file-path {
@@ -152,53 +234,81 @@ $sourceFiles = $sourceFiles = generateSourceFiles(module: $module);
   }
   
   #EntityTypeExplorer div.entity-type-code-preview pre code {
-    font-size: 0.7rem;
-    backdrop-filter: brightness(0.8) blur(50px);
-    display: block;
-    height: 10rem;
-    overflow: scroll;
-    padding: 0.5rem;
+    width: 100%;
   }
 
-  #EntityTypeDetail {
+  #EntityTypeExplorer div.entity-type-explorer div.file-browser {
+    max-width: 20vw;
+    overflow: scroll;
+    backdrop-filter: brightness(0.35);
+    display: flex;
+    flex-direction: column;
+    box-shadow: inset 1px 1px 10px rgba(0,0,0,0.25);
+    padding: 0.25rem 0;
+  }
+
+  #EntityTypeExplorer div.entity-type-explorer div.file-browser div.file-browser-file {
+    padding: 0.5rem 0.75rem;
+    box-sizing: border-box;
+    font-size: 0.9rem;
+    display: flex;
+    flex-direction: column;
+    color: rgba(255, 255, 255, 0.75);
+    font-weight: 100;
+    cursor: pointer;
+  }
+
+  #EntityTypeExplorer div.entity-type-explorer div.file-browser div.file-browser-file:hover {
+    color: rgba(255,255,255,1);
+    background: linear-gradient(to bottom, rgba(0,0,0,0.25) 0%, rgba(0,0,0,0.5) 100%);
+  }
+
+  #EntityTypeExplorer h2 {
+    margin: 0;
+    padding: 1rem;
+    backdrop-filter: brightness(0.5);
+  }
+
+  section.entity-type-admin-block {
     backdrop-filter: brightness(0.5);
     border-radius: 0.5rem;
     overflow: hidden;
+    margin-bottom: 2rem;
   }
-  #EntityTypeDetail h2, 
-  #EntityTypeDetail h3,
-  #EntityTypeDetail ul {
+  section.entity-type-admin-block h2, 
+  section.entity-type-admin-block h3,
+  section.entity-type-admin-block ul {
     padding: 1rem 1.5rem 0;
   }
-  #EntityTypeDetail h2, 
-  #EntityTypeDetail h3 {
+  section.entity-type-admin-block h2, 
+  section.entity-type-admin-block h3 {
     font-size: 1.2rem;
     margin-top: 0;
     margin-bottom: 0;
   }
-  #EntityTypeDetail h2 {
+  section.entity-type-admin-block h2 {
     
   }
-  #EntityTypeDetail h3 {
+  section.entity-type-admin-block h3 {
     margin-top: 0;
   }
-  #EntityTypeDetail h1 span {
-    color: var(--color);
+  h1 span {
+    font-weight: 200;
   }
   
-  #EntityTypeDetail ul {
+  section.entity-type-admin-block ul {
     list-style-type: none;
     border-top: 1px solid;
     font-size: 0.9rem;
     line-height: 2;
   }
   
-  #EntityTypeDetail ul li {
+  section.entity-type-admin-block ul li {
     display: flex;
     flex-direction: column;
   }
   
-  #EntityTypeDetail ul li span {
+  section.entity-type-admin-block ul li span {
     font-size: 0.7rem;
     color: rgba(255,255,255,0.5);
   }
@@ -220,8 +330,32 @@ $sourceFiles = $sourceFiles = generateSourceFiles(module: $module);
     }
 
     section.split-content div.content-section section {
-
     }
+
+    #EntityTypeExplorer div.entity-type-explorer {
+      flex-direction: row-reverse;
+      justify-content: flex-end;
+      align-items: stretch;
+    }
+
+    #EntityTypeExplorer div.entity-type-explorer div.file-view {
+      display: flex;
+      flex-direction: column;
+      align-items: stretch;
+      justify-content: stretch;
+      flex: 1;
+    }
+
+    #EntityTypeExplorer div.entity-type-explorer div.file-browser {
+      max-width: 20vw;
+    }
+
+    #EntityTypeExplorer div.entity-type-code-preview pre {
+      max-height: 80vh;
+      max-width: 41vw;
+    }
+
+
 
     
   }
